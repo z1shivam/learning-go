b22c1a61baeb297b9830d3c4bc63dfe9c0d797e8
z1shivam <shivamk.services@gmail.com>
2024-09-14 12:31:15 +0530

    slices

--- 08_slices/main.go
+++ 08_slices/main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("heyy")
+
+	var mySlice = []string{}
+
+	mySlice = append(mySlice, "firstItem", "secondItem", "thirdItem")
+	mySlice = append(mySlice[2:], "fourthItem")
+	mySlice = mySlice[1:]
+
+	highScores := make([]int, 4)
+	highScores[0] = 23
+	highScores[1] = 32
+	highScores[2] = 34
+	highScores[3] = 43
+	// highScores[4] = 55
+
+	fmt.Println(highScores)
+	fmt.Println(mySlice)
 }
